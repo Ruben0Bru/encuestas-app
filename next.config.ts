8b3e7f3a02a9c1267d37a160891c6010fcc97197
Nemo <rbruherrera48@correo.unicordoba.edu.co>
2025-11-27 13:31:00 -0500

    fix: Cambiar config a MJS para evitar errores de compilacion

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
-import type { NextConfig } from "next";
-
-// Al poner ': any', le decimos a VS Code que no moleste con las reglas
-const nextConfig: any = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: {
+    // Ignorar errores de ESLint durante el build
     ignoreDuringBuilds: true,
   },
   typescript: {
+    // Ignorar errores de TypeScript durante el build
     ignoreBuildErrors: true,
   },
 };
